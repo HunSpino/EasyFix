00b1dce8ecc9be4e0201d221227a647c1aa3db40
HunSpino <tamasleventelaszlo99@gmail.com>
2022-03-01 13:23:54 +0100

    idegenkulcsok megvannak

--- database/migrations/2022_03_01_114221_create_hibajelents_table.php
+++ database/migrations/2022_03_01_114221_create_hibajelents_table.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Models\Allapot;
+use App\Models\Gepek;
+use App\Models\Hibajelent;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration
 {
@@ -15,7 +20,18 @@ return new class extends Migration
     {
         Schema::create('hibajelents', function (Blueprint $table) {
             $table->id();
+            $table->dateTime('datum');
+            $table->string('hiba');
             $table->timestamps();
+        });
+        Schema::table('hibajelents',function (Blueprint $table){
+            $table->foreignIdFor(User::class)->constrained();
+        });
+        Schema::table('hibajelents',function (Blueprint $table){
+            $table->foreignIdFor(Gepek::class)->constrained();
+        });
+        Schema::table('hibajelents',function (Blueprint $table){
+            $table->foreignIdFor(Allapot::class)->constrained();
         });
     }
 
@@ -26,6 +42,16 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::table('hibajelents',function (Blueprint $table){
+            $table->dropForeignIdFor(User::class);
+        });
+        Schema::table('hibajelents',function (Blueprint $table){
+            $table->dropForeignIdFor(Gepek::class);
+        });
+        Schema::table('hibajelents',function (Blueprint $table){
+            $table->dropForeignIdFor(Allapot::class);
+        });
+
         Schema::dropIfExists('hibajelents');
     }
 };
