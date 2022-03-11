252cc22133da201b0cdf7f45429979fd8d91c9c3
HunSpino <tamasleventelaszlo99@gmail.com>
2022-03-11 14:21:48 +0100

    Az Edit működik

---
+++ resources/views/hibajelents/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('title','Szerkesztés')
+
+@section('content')
+<form method='POST' action="{{ route('hibajelents.update', $hibajelent->id) }}">
+        @method('PATCH')
+        @csrf
+        <div>
+            A jelenteni való hiba: <br>
+            <input type="text" name="hiba" value="{{ $hibajelent->hiba }}">
+        </div>
+        <div>
+            A hibajelentő felhasználó "ID"-ja: <br>
+            <input type="number" name="user_id" value="{{ $hibajelent->user_id }}">
+        </div>
+        <div>
+            A hibát tartalmazó gép "ID"-ja: <br>
+            <input type="number" name="gepek_id" value="{{ $hibajelent->gepek_id }}">
+        </div>
+        <div>
+            Állapot: <br>
+            <input type="number" name="allapot_id" value="{{ $hibajelent->allapot_id}}">
+        </div>
+        <div>
+            <input type="submit" value="Edit">
+            <a href="{{ route('hibajelents.show',[$hibajelent->id]) }}"><button type="button">Mégse</button></a>
+        </div>
+
+@endsection
