a543b7ca2044a17717c1b6317b0111fd0787d81d
HunSpino <tamasleventelaszlo99@gmail.com>
2022-03-22 12:04:53 +0100

    laravel breeze hozzáadva

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ HomeController::class, 'index' ])->name('home');
 Route::resource('/gepeks',GepekController::class);
 Route::resource('/hibajelents',HibajelentController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
