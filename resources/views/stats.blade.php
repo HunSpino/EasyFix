4f23625c7d48abd8b6d6e26580884b9947a95bf6
HunSpino <tamasleventelaszlo99@gmail.com>
2022-03-22 13:26:13 +0100

    A login and reqist az index page-en

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -6,10 +6,19 @@
 <h1>Üdvözöllek a kezdőoldalon!</h1>
 <p>Ennyi személlyel dolgozik a cégünk: {{ $userCount }}</p>
 <p>Ennyi géppel dolgozik cégünk: {{ $gepekCount }}</p>
+@auth
+    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
 <p>
     <a href="{{ route('gepeks.index') }}"><button type="button">Gépek amink vannak</button></a>
 </p>
 <p>
     <a href="{{ route('hibajelents.index') }}"><button type="button">Hibabejelentések</button></a>
 </p>
+@else
+<a href="{{ route('login') }}"><button type="button">Log in</button></a>
+
+@if (Route::has('register'))
+    <a href="{{ route('register') }}"><button type="button">Register</button></a>
+    @endif
+@endauth
 @endsection
